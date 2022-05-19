eccee039d9b1e12da6460e277ea61b92dbbf0e31
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2022-05-19 14:24:30 +0700

    first commit

---
+++ src/state/actions/index.ts
@@ -0,0 +1,26 @@
+import { ActionType } from "../constants";
+
+export interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+export type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
